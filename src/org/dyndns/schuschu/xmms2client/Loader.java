c3fed5d3fd40b82673a44d338de806cc08ad7d74
schuschu <schuschu@cabal>
2010-02-02 03:42:38 +0100

    playing with git again

--- src/org/dyndns/schuschu/xmms2client/Loader.java
+++ src/org/dyndns/schuschu/xmms2client/Loader.java
@@ -19,6 +19,7 @@ public class Loader {
 		
 		// TODO: add debug flag
 		// TODO: get a git howto
+		// TODO: find tims super duper merging tool
 
 		// set default host
 		String host = new String("127.0.0.1");
