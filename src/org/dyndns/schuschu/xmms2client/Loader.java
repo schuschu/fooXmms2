e14b699a00df7954c92c94ad76788f4e61bb14ad
schuschu <akku1@gmx.at>
2010-02-03 02:38:27 +0100

    Fixed that annoying behavior that hides the window from me (HAHAHA it is a standard because i declared it so ... i should drink less coffee...)

--- src/org/dyndns/schuschu/xmms2client/Loader.java
+++ src/org/dyndns/schuschu/xmms2client/Loader.java
@@ -7,67 +7,79 @@ import se.fnord.xmms2.client.ClientFactory;
 
 /**
  * @author schuschu
- *
+ * 
  */
 public class Loader {
-	
+
 	/**
-	 * parses command line arguments
-	 * initializes main window
+	 * parses command line arguments initializes main window
 	 * 
 	 * @param args
 	 */
-	public static void main (String[] args) {
-		
+	public static void main(String[] args) {
+
 		// TODO: add debug flag
-		// TODO: add initial state flag (in tray or start normal)
 
 		// set default host
 		String host = new String("127.0.0.1");
 		// set default port
 		int port = 9667;
+		// set default initial state of the window
+		boolean hidden = false;
 
 		// parsing command line arguments
 		// if argument is invalid, drop user shit and assume default
 		for (int run = 0; run < args.length; run++) {
 			String trial_host = null;
 
-			// check whether host has been explity specified 
+			// check whether host has been explity specified
 			if (args[run].equals("--host") || args[run].equals("-h")) {
-				trial_host = new String (args[run + 1]);
+				trial_host = new String(args[run + 1]);
 
 				if (validIPAddress(trial_host)) {
 					host = trial_host;
 				}
 			}
-			
-			// check whether port has been explity specified 
+
+			// check whether port has been explity specified
 			if (args[run].equals("--port") || args[run].equals("-p")) {
 				int trial_port = 0;
 
 				try {
 					trial_port = Integer.parseInt(args[run + 1]);
+				} catch (NumberFormatException e) {
+				} catch (ArrayIndexOutOfBoundsException e) {
 				}
-				catch (NumberFormatException e) {}
-				catch (ArrayIndexOutOfBoundsException e) {}
 
 				// check port range
 				if ((trial_port > 0) && (trial_port < 65535)) {
 					port = trial_port;
 				}
 			}
+
+			// check if the window is supposed to go into tray directly
+			if (args[run].equals("--hiden") || args[run].equals("-h")) {
+				hidden = true;
+			}
 		}
 
-		Client client = ClientFactory
-				.create("fooXmms2", "tcp://" + host + ":" + port);
+		// TODO: support other protocolls
+		Client client = ClientFactory.create("fooXmms2", "tcp://" + host + ":"
+				+ port);
 
 		client.start();
 
 		FooPluginWindowDefault main = new FooPluginWindowDefault(client);
 
 		if (SystemTray.isSupported()) {
+			// TODO: do something about this warning, suppressing it looks
+			// bad
 			@SuppressWarnings("unused")
-			FooPluginViewTrayicon trayicon = new FooPluginViewTrayicon(main, client);
+			FooPluginViewTrayicon trayicon = new FooPluginViewTrayicon(main,
+					client);
+			if (!hidden) {
+				main.setVisible(true);
+			}
 		} else {
 			main.setVisible(true);
 		}
@@ -79,21 +91,21 @@ public class Loader {
 	 * @param ip
 	 * @return boolean
 	 */
-	public final static boolean validIPAddress (String ip) {
-	    String[] octets = ip.split("\\.");
+	public final static boolean validIPAddress(String ip) {
+		String[] octets = ip.split("\\.");
 
-	    if (octets.length != 4) {
-	        return false;
-	    }
+		if (octets.length != 4) {
+			return false;
+		}
 
-	    for (String octet : octets) {
-	        int i = Integer.parseInt(octet);
+		for (String octet : octets) {
+			int i = Integer.parseInt(octet);
 
-	        if ((i < 0) || (i > 255)) {
-	            return false;
-	        }
-	    }
+			if ((i < 0) || (i > 255)) {
+				return false;
+			}
+		}
 
-	    return true;
+		return true;
 	}
 }
