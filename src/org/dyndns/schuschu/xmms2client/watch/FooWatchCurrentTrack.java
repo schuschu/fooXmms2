78ed55d7b96f298c34b46687ec7939a78942a78b
schuschu <akku1@gmx.at>
2010-02-10 23:48:35 +0100

    removed check for to many events (try switching THAT fast)

--- src/org/dyndns/schuschu/xmms2client/watch/FooWatchCurrentTrack.java
+++ src/org/dyndns/schuschu/xmms2client/watch/FooWatchCurrentTrack.java
@@ -8,12 +8,12 @@ import se.fnord.xmms2.client.commands.Command;
 import se.fnord.xmms2.client.commands.Playback;
 
 public class FooWatchCurrentTrack extends Thread {
-	
+
 	private static final boolean DEBUG = FooLoader.DEBUG;
-	private String name="FooWatchCurrentTrack";
-	
-	private void debug(String message){
-		if(DEBUG){
+	private String name = "FooWatchCurrentTrack";
+
+	private void debug(String message) {
+		if (DEBUG) {
 			System.out.println("debug: " + name + " " + message);
 		}
 	}
@@ -45,19 +45,12 @@ public class FooWatchCurrentTrack extends Thread {
 	}
 
 	public void run() {
-		// TODO: find better way to prevent refresh after the insertion/deletion
-		// of each track (they are NOT removed at once!)
-		long x = 0;
-		long y = 0;
+
 		running = true;
 		while (running) {
 			try {
-				x = System.currentTimeMillis();
 				current = c.waitReply();
-				y = System.currentTimeMillis();
-				if (y - x > 100) {
-					view.getReal().getDisplay().asyncExec(r);
-				}
+				view.getReal().getDisplay().asyncExec(r);
 			} catch (InterruptedException e) {
 				Thread.currentThread().interrupt();
 			}
