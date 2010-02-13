ccdcd5d6a1fb8b12b1667b58e687e6ad7eb9205d
schuschu <akku1@gmx.at>
2010-02-13 17:04:02 +0100

    improved foodebug

--- src/org/dyndns/schuschu/xmms2client/debug/FooDebug.java
+++ src/org/dyndns/schuschu/xmms2client/debug/FooDebug.java
@@ -1,5 +1,8 @@
 package org.dyndns.schuschu.xmms2client.debug;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -15,12 +18,18 @@ import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 
 public class FooDebug extends OutputStream {
+
+	// TODO: replace with fooview elements?
 
 	private static final int defForeground = SWT.COLOR_BLACK;
 	private static final int defBackground = SWT.COLOR_WHITE;
@@ -53,7 +62,7 @@ public class FooDebug extends OutputStream {
 		setBackground(defBackground);
 
 		createSShell();
-		
+
 		Runnable runable = new Runnable() {
 
 			@Override
@@ -75,7 +84,6 @@ public class FooDebug extends OutputStream {
 										.getSystemColor(buffer.get(0)
 												.getForeground()));
 								buffer.remove(0);
-								//table.getColumn(0).pack();
 								if (!buttonScroll.getSelection()) {
 									table.showItem(item);
 								}
@@ -189,8 +197,76 @@ public class FooDebug extends OutputStream {
 		buttonArea.setLayoutData(buttonData);
 
 		buttonScroll = new Button(buttonArea, SWT.TOGGLE);
-
 		buttonScroll.setText("Scroll-lock");
+
+		Button buttonClear = new Button(buttonArea, SWT.NONE);
+		buttonClear.addListener(SWT.Selection, new Listener() {
+
+			@Override
+			public void handleEvent(Event arg0) {
+				table.clearAll();
+				table.setItemCount(0);
+
+			}
+		});
+		buttonClear.setText("Clear");
+
+		Button buttonSave = new Button(buttonArea, SWT.NONE);
+		buttonSave.addListener(SWT.Selection, new Listener() {
+
+			@Override
+			public void handleEvent(Event arg0) {
+				FileDialog fd = new FileDialog(sShell, SWT.SAVE);
+				fd.setText("Save logfile");
+				fd.setFileName("fooXmms2.log");
+				String selected = fd.open();
+
+				if (selected != null) {
+					try {
+						File file = new File(selected);
+
+						boolean write = true;
+
+						if (!file.createNewFile()) {
+							MessageBox mb = new MessageBox(sShell,
+									SWT.ICON_WARNING | SWT.YES | SWT.NO);
+
+							mb.setText("Replace file");
+							mb
+									.setMessage(file.getName()
+											+ " already exists. Do you want to replace it?");
+
+							write = mb.open() == SWT.YES;
+						}
+
+						if (write) {
+							if (file.canWrite()) {
+								FileWriter fstream = new FileWriter(file);
+								BufferedWriter out = new BufferedWriter(fstream);
+								for (TableItem item : table.getItems()) {
+									out.write(item.getText() + "\n");
+								}
+								out.close();
+							} else {
+								MessageBox mb = new MessageBox(sShell,
+										SWT.ICON_ERROR | SWT.OK);
+								mb.setText("Error");
+								mb.setMessage("Can not write to file!");
+								mb.open();
+							}
+						}
+					} catch (IOException e) {
+						MessageBox mb = new MessageBox(sShell,
+								SWT.ICON_ERROR | SWT.OK);
+						mb.setText("Error");
+						mb.setMessage(e.getMessage());
+						mb.open();
+					}
+				}
+
+			}
+		});
+		buttonSave.setText("Save");
 	}
 
 	public static void setForeground(int foreground) {
