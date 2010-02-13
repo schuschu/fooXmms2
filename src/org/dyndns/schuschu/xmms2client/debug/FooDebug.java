90be4ea65752c2da048e33ae1c2a825eb49a6a7c
schuschu <akku1@gmx.at>
2010-02-13 02:23:56 +0100

    threaded debug console

--- src/org/dyndns/schuschu/xmms2client/debug/FooDebug.java
+++ src/org/dyndns/schuschu/xmms2client/debug/FooDebug.java
@@ -3,6 +3,7 @@ package org.dyndns.schuschu.xmms2client.debug;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Vector;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
@@ -14,7 +15,7 @@ import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 
-public class FooDebug extends OutputStream {
+public class FooDebug extends OutputStream implements Runnable {
 
 	private static final int defForeground = SWT.COLOR_BLACK;
 	private static final int defBackground = SWT.COLOR_WHITE;
@@ -22,35 +23,69 @@ public class FooDebug extends OutputStream {
 	private static int foreground;
 	private static int background;
 
+	private boolean run = true;
+
 	private Shell sShell;
 	private Display display;
 	private Table table;
 
 	private StringBuffer sb;
 
+	private Vector<BufferEntry> buffer;
+
 	public FooDebug() {
 
 		this.display = Display.getDefault();
+		buffer = new Vector<BufferEntry>();
 		sb = new StringBuffer();
 
 		setForeground(defForeground);
 		setBackground(defBackground);
 
 		createSShell();
+
+		Thread self = new Thread(this);
+		self.start();
+
+	}
+
+	public void done() {
+		run = false;
 	}
 
 	private void writeline() {
-		if (sShell.isDisposed()) {
-			createSShell();
-		}
-		TableItem item = new TableItem(table, SWT.NONE);
-		item.setText(sb.toString());
-		item.setBackground(display.getSystemColor(getBackground()));
-		item.setForeground(display.getSystemColor(getForeground()));
+		BufferEntry entry = new BufferEntry();
+		entry.setBackground(background);
+		entry.setForeground(foreground);
+		entry.setText(sb.toString());
+		sb = new StringBuffer();
+		buffer.add(entry);
 		setForeground(defForeground);
 		setBackground(defBackground);
-		sb = new StringBuffer();
-		table.getColumn(0).pack();
+	}
+
+	@Override
+	public void run() {
+		while (run) {
+			if (buffer.size() != 0) {
+				display.syncExec(new Runnable() {
+					public void run() {
+						if (sShell.isDisposed()) {
+							createSShell();
+						}
+						TableItem item = new TableItem(table, SWT.NONE);
+						item.setText(buffer.get(0).getText());
+						item.setBackground(display.getSystemColor(buffer.get(0)
+								.getBackground()));
+						item.setForeground(display.getSystemColor(buffer.get(0)
+								.getForeground()));
+						buffer.remove(0);
+						table.getColumn(0).pack();
+					}
+				});
+
+			}
+		}
 	}
 
 	@Override
@@ -104,15 +139,39 @@ public class FooDebug extends OutputStream {
 		FooDebug.foreground = foreground;
 	}
 
+	public static void setBackground(int background) {
+		FooDebug.background = background;
+	}
+
+}
+
+class BufferEntry {
+	private int foreground;
+	private int background;
+	private String text;
+
+	public void setForeground(int foreground) {
+		this.foreground = foreground;
+	}
+
 	public int getForeground() {
 		return foreground;
 	}
 
-	public static void setBackground(int background) {
-		FooDebug.background = background;
+	public void setBackground(int background) {
+		this.background = background;
 	}
 
 	public int getBackground() {
 		return background;
 	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
 }
