a2d8cdaad7ac4cfe10016013a5848870a2cdb3f1
schuschu <akku1@gmx.at>
2010-02-25 23:18:13 +0100

    cleanup

--- src/org/dyndns/schuschu/xmms2client/factories/FooWatchType.java
+++
@@ -1,5 +0,0 @@
-package org.dyndns.schuschu.xmms2client.factories;
-
-public enum FooWatchType {
-	FooWatchCurrentTrack, FooWatchPlaybackPos, FooWatchPlaybackStatus, FooWatchPlaylist, FooWatchPlaylistLoad;
-}
