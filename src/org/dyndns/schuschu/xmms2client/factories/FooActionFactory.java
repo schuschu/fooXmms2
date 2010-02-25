d93010aaad1c3d4d7ca23836c58215f1f19154e7
schuschu <akku1@gmx.at>
2010-02-25 04:26:16 +0100

    some sort of pluginarchitecture

--- src/org/dyndns/schuschu/xmms2client/factories/FooActionFactory.java
+++ src/org/dyndns/schuschu/xmms2client/factories/FooActionFactory.java
@@ -1,10 +1,9 @@
 package org.dyndns.schuschu.xmms2client.factories;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 
-import org.dyndns.schuschu.xmms2client.action.FooPlayback;
-import org.dyndns.schuschu.xmms2client.action.FooPlaylist;
-import org.dyndns.schuschu.xmms2client.action.FooSystem;
 import org.dyndns.schuschu.xmms2client.action.base.FooAction;
 import org.dyndns.schuschu.xmms2client.action.base.FooKey;
 import org.dyndns.schuschu.xmms2client.action.base.FooSource;
@@ -14,6 +13,8 @@ import org.dyndns.schuschu.xmms2client.interfaces.view.FooInterfaceAction;
 import org.dyndns.schuschu.xmms2client.loader.FooLoader;
 import org.dyndns.schuschu.xmms2client.loader.FooXML;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class FooActionFactory {
 	private static final boolean DEBUG = FooLoader.DEBUG;
@@ -34,11 +35,45 @@ public class FooActionFactory {
 
 	public FooActionFactory() {
 
-		// TODO: dynamic vodoo
-		FooPlayback.registerActionFactory();
-		FooPlaylist.registerActionFactory();
-		FooSystem.registerActionFactory();
+		// TODO: sudo make it good
+		try {
+			Element plugins = FooXML.getElement("plugins");
 
+			NodeList children = plugins.getChildNodes();
+
+			for (int i = 0; i < children.getLength(); i++) {
+				Node node = children.item(i);
+
+				if (node.getNodeName().equals("plugin")) {
+
+					Element element = (Element) node;
+					String plugin = element.getAttribute("name");
+
+					Class<?> clazz = Class.forName(plugin);
+					Method meth = clazz.getMethod("registerActionFactory");
+					meth.invoke(clazz);
+				}
+			}
+
+		} catch (SecurityException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 	public FooAction create(Element element) {
